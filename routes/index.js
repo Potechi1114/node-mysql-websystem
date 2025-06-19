c8cc90926fe3becbeda2f7773ea5f9c250dfb47e
Potechi1114 <kw-1114@au.com>
2025-06-19 06:35:33 +0000

    chapter11

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'todo_app'
-});
+let todos = [];
 
 router.get('/', function (req, res, next) {
-  connection.query(
-    `select * from tasks;`,
-    (error, results) => {
-      console.log(error);
-      console.log(results);
-      res.render('index', {
-        title: 'ToDo App',
-        todos: results,
-      });
-    }
-  );
+  res.render('index', {
+    title: 'ToDo App',
+    todos: todos,
+  });
 });
 
 router.post('/', function (req, res, next) {
-  connection.connect((err) => {
-    if (err) {
-      console.log('error connecting: ' + err.stack);
-      return
-    }
-    console.log('success');
-  });
   const todo = req.body.add;
-  connection.query(
-    `insert into tasks (user_id, content) values (1, '${todo}');`,
-    (error, results) => {
-      console.log(error);
-      res.redirect('/');
-    }
-  );
+  todos.push(todo);
+  res.redirect('/');
 });
 
 module.exports = router;
